6b23830e526c53bd28a8235209b12302195792a2
Sergey1-git <Sergey1-git.yan.dex.ru@yandex.ru>
2025-09-29 12:59:55 +0300

    create file time.html and styles.css.html add into api_flask.py func time and func correct_input and add into date_flask.py func date_flask

--- api_flask.py
+++ api_flask.py
@@ -1,16 +1,48 @@
 import pandas as pd
 from flask import Flask, render_template, url_for,request, flash
+from date_flask import data_flask
+import re
 
 
 app = Flask(__name__)
 
-menu = [{"name": "Главная страница", "url": "/"}]
+app.config['SECRET_KEY'] = 'gdgas43gdfgfgs46hfg6hf7gh9f'
 
+menu = [{"name": "Главная страница", "url": "/"},
+        {"name": f"Получение данных на указанное время", "url": "time"}]
+
+
+
+def correct_input(string):
+    if 15<=len(string)<=16:
+        is_valid = re.search (r"[0-2]{1}\d{1}\.[0-1]{1}\d{1}\.2025\s[0-2]?[0-9]{1}:[0-5]{1}[0-9]{1,1}$", string)
+        if is_valid:
+            return True
+        else:
+            return False
+    else:
+        return False
 
 @app.route("/",methods=['GET'])
 def index():
 
     return render_template('index.html',title="О сайте", menu=menu)
+
+
+@app.route("/time" , methods=['GET','POST'])
+def time():
+
+    result_time = ''
+    if request.method == 'POST':
+        date1 = request.form['date1']
+        if date1 !='' :
+            if correct_input(date1) is True:
+                result_time = data_flask(date1)
+            else:
+                flash(f'Данные {date1} не соответствуют требуемому формату, повторите ввод.')
+        else:
+            flash('Поле ввода данных не заполнено.')
+    return render_template('time.html', title="Запрос данных по времени.", menu=menu, result=result_time)
 
 
 
--- date_flask.py
+++ date_flask.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import datetime
+from datetime import datetime
+from datetime import timedelta
 
 try:
     # Попробуйте прочитать CSV-файл
@@ -14,3 +17,19 @@ df['Datetime'] = pd.to_datetime(df['Datetime'], format='%Y-%m-%d %H:%M')
 df['Price'] = pd.to_numeric(df['Price'], errors='coerce')
 df=df.sort_values(by='Datetime')
 df=df.reset_index(drop=True)
+
+
+def data_flask(date1, date2=None):
+    if date2 is None:
+        value_day_minute = datetime.strptime(date1, "%d.%m.%Y %H:%M")
+        value_day_minute_min = datetime.strptime(date1, "%d.%m.%Y %H:%M") - timedelta(minutes=2.1)
+        value_day_minute_max = datetime.strptime(date1, "%d.%m.%Y %H:%M") + timedelta(minutes=2.1)
+        day_minute_one = df[(df['Datetime'] > value_day_minute_min) & (df['Datetime'] < value_day_minute_max)]
+
+        if (day_minute_one['Datetime'] == value_day_minute).any():
+            day_minute_one1 = day_minute_one[(day_minute_one['Datetime'] == value_day_minute)]
+            day_minute_one1 = day_minute_one1.to_html()
+            return day_minute_one1
+        else:
+            day_minute_one = day_minute_one.to_html()
+            return day_minute_one
